5abb0f5367788a95dd981097e0cf5631b922d559
Victor <viktorstupin9@gmail.comgit config --global user.email viktorstupin9@mail.com>
2025-08-10 21:27:54 +0300

    add task solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,29 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    const length = this.length;
+
+    for (let i = 0; i < length - 1; i++) {
+      for (let j = 0; j < length - 1 - i; j++) {
+        let shouldSwap;
+
+        if (compareFunction) {
+          shouldSwap = compareFunction(this[j], this[j + 1]) > 0;
+        } else {
+          shouldSwap = String(this[j]) > String(this[j + 1]);
+        }
+
+        if (shouldSwap) {
+          const temp = this[j];
+
+          this[j] = this[j + 1];
+          this[j + 1] = temp;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
