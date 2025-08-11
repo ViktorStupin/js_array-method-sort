0c1978be1573694cc99edf5b5f8c772bb1224a2d
Victor <viktorstupin9@gmail.comgit config --global user.email viktorstupin9@mail.com>
2025-08-11 11:03:54 +0300

    add task solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -7,23 +7,47 @@ function applyCustomSort() {
   [].__proto__.sort2 = function (compareFunction) {
     const length = this.length;
 
-    for (let i = 0; i < length - 1; i++) {
-      for (let j = 0; j < length - 1 - i; j++) {
+    // Helper function to check if a value should be moved to the end
+    const isUndefinedOrEmpty = (value) =>
+      value === undefined || (!(value in this) && typeof value === 'undefined');
+
+    // First pass: Move undefined and empty slots to the end
+    const validElements = [];
+
+    for (let i = 0; i < length; i++) {
+      if (!isUndefinedOrEmpty(this[i])) {
+        validElements.push(this[i]);
+      }
+    }
+
+    // Sort valid elements
+    for (let i = 0; i < validElements.length - 1; i++) {
+      for (let j = 0; j < validElements.length - 1 - i; j++) {
         let shouldSwap;
 
         if (compareFunction) {
-          shouldSwap = compareFunction(this[j], this[j + 1]) > 0;
+          shouldSwap =
+            compareFunction(validElements[j], validElements[j + 1]) > 0;
         } else {
-          shouldSwap = String(this[j]) > String(this[j + 1]);
+          shouldSwap = String(validElements[j]) > String(validElements[j + 1]);
         }
 
         if (shouldSwap) {
-          const temp = this[j];
-
-          [this[j], this[j + 1]] = [this[j + 1], this[j]];
-          this[j + 1] = temp;
+          [validElements[j], validElements[j + 1]] = [
+            validElements[j + 1],
+            validElements[j],
+          ];
         }
       }
+    }
+
+    // Reconstruct the array with sorted elements followed by undefined
+    for (let i = 0; i < validElements.length; i++) {
+      this[i] = validElements[i];
+    }
+
+    for (let i = validElements.length; i < length; i++) {
+      this[i] = undefined;
     }
 
     return this;
